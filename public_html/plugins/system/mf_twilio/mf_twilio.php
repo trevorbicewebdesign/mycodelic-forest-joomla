13aa19f56bbe03bfafb3ee7b267324ae15d12612
Trevor Bice <trevorbicewebdesign@gmail.com>
2019-06-23 19:50:58 -0700

    Update mf_twilio.php

--- public_html/plugins/system/mf_twilio/mf_twilio.php
+++ public_html/plugins/system/mf_twilio/mf_twilio.php
@@ -24,10 +24,10 @@ class plgSystemMf_twilio extends JPlugin
 		
 		if($task == 'twilio.insertSlack' ){
 			
-			require_once('/home/trevorbi/mycodelicforest.org/public_html/administrator/components/com_civicrm/civicrm/api/class.api.php');
+			require_once(JPATH_ROOT."/administrator/components/com_civicrm/civicrm/api/class.api.php");
 			$api = new civicrm_api3(array(
 			  // Specify location of "civicrm.settings.php".
-			  'conf_path' => '/home/trevorbi/mycodelicforest.org/public_html/administrator/components/com_civicrm/',
+			  'conf_path' => JPATH_ROOT."/administrator/components/com_civicrm/",
 			));
 			echo $from;
 			
