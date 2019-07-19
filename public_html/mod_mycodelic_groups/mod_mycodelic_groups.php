d649f0f439a4310e4bdf9af2f1f2e9cabaef2804
Trevor Bice <trevorbicewebdesign@gmail.com>
2019-07-18 19:57:24 -0700

    Add mycodelic groups module

---
+++ public_html/mod_mycodelic_groups/mod_mycodelic_groups.php
@@ -0,0 +1,64 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+require_once('/home/trevorbi/mycodelicforest.org/public_html/administrator/components/com_civicrm/civicrm/api/class.api.php');
+$api = new civicrm_api3(array(
+  // Specify location of "civicrm.settings.php".
+  'conf_path' => '/home/trevorbi/mycodelicforest.org/public_html/administrator/components/com_civicrm/',
+));  
+
+$apiParams = array(
+  'email' => $_POST['form']['email']
+ 
+);
+
+// print_r($result);
+
+echo "<h3>Leadership</h3>";
+$result = civicrm_api3('Contact', 'get', array(   
+  'return' => "id",   
+  'filter.group_id' => array(0 => 11)
+));
+foreach($result['values'] as $index=>$val){
+	$apiParams = array(
+	  'id' => $val['contact_id']
+	);
+	if ($api->Contact->Get($apiParams)) {
+	  //each key of the result array is an attribute of the api
+		$civiUser = $api->lastResult->values[0];
+	}
+	echo "<div class='col-lg-3'>".$civiUser->display_name."</div>";	
+}
+
+echo "<h3>2018</h3>";
+$result = civicrm_api3('Contact', 'get', array(   
+  'return' => "id",   
+  'filter.group_id' => array(0 => 9)
+));
+foreach($result['values'] as $index=>$val){
+	$apiParams = array(
+	  'id' => $val['contact_id']
+	);
+	if ($api->Contact->Get($apiParams)) {
+	  //each key of the result array is an attribute of the api
+		$civiUser = $api->lastResult->values[0];
+	}
+	echo "<div class='col-lg-3'>".$civiUser->display_name."</div>";	
+}
+
+echo "<h3>Newbiews</h3>";
+$result = civicrm_api3('Contact', 'get', array(   
+  'return' => "id",   
+  'filter.group_id' => array(0 => 7)
+));
+foreach($result['values'] as $index=>$val){
+	$apiParams = array(
+	  'id' => $val['contact_id']
+	);
+	if ($api->Contact->Get($apiParams)) {
+	  //each key of the result array is an attribute of the api
+		$civiUser = $api->lastResult->values[0];
+	}
+	echo "<div class='col-lg-3'>".$civiUser->display_name."</div>";	
+}
+
+?>
