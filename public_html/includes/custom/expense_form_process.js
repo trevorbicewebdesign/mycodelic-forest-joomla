efd763002deab0a4f46fef62c04d3824590de554
Trevor Bice <trevorbicewebdesign@gmail.com>
2019-09-05 16:49:48 -0700

    calculate tax

--- public_html/includes/custom/expense_form_process.js
+++ public_html/includes/custom/expense_form_process.js
@@ -7,7 +7,7 @@ jQuery( document ).ready(function() {
          //jQuery(this).val(total.toFixed(2));
      });
 */
-     jQuery( "#item_amount_1,#item_amount_2,#item_amount_3,#item_amount_4" ).keyup(function() {
+     jQuery( "#item_amount_1,#item_amount_2,#item_amount_3,#item_amount_4,#tax" ).keyup(function() {
          var total = addAmountFields();
          jQuery("#item_total").val( total.toFixed(2) );
      });
@@ -19,7 +19,7 @@ function addAmountFields(){
     sum += parseFloat(jQuery( "#item_amount_2").val())?parseFloat(jQuery( "#item_amount_2").val()):0;
     sum += parseFloat(jQuery( "#item_amount_3").val())?parseFloat(jQuery( "#item_amount_3").val()):0;
     sum += parseFloat(jQuery( "#item_amount_4").val())?parseFloat(jQuery( "#item_amount_4").val()):0;
-    
+    sum += parseFloat(jQuery( "#tax").val())?parseFloat(jQuery( "#tax").val()):0;
     
     return sum;
 }
