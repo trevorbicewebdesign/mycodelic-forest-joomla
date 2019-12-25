8668030ea4d4631e795d00fe39d54d129977aaf6
Git User <git@localhost>
2019-12-24 23:49:41 -0600

    Update T3 System Plugin from v2.7.4 to v2.7.5

--- public_html/media/civicrm/templates_c/en_US/%%87/870/8709C7E9%%string%3ADear+Michelle.php
+++
@@ -1,5 +0,0 @@
-<?php /* Smarty version 2.6.31, created on 2019-12-22 20:07:19
-         compiled from string:Dear+Michelle */ ?>
-<?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
-smarty_core_load_plugins(array('plugins' => array(array('block', 'crmScope', 'string:Dear Michelle', 1, false),)), $this); ?>
-<?php $this->_tag_stack[] = array('crmScope', array('extensionKey' => "")); $_block_repeat=true;smarty_block_crmScope($this->_tag_stack[count($this->_tag_stack)-1][1], null, $this, $_block_repeat);while ($_block_repeat) { ob_start(); ?>Dear Michelle<?php $_block_content = ob_get_contents(); ob_end_clean(); $_block_repeat=false;echo smarty_block_crmScope($this->_tag_stack[count($this->_tag_stack)-1][1], $_block_content, $this, $_block_repeat); }  array_pop($this->_tag_stack); ?>
