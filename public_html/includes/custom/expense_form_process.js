e4e9ebac633a07ed0f40c3561f29ffa9fc00b246
Trevor Bice <trevorbicewebdesign@gmail.com>
2019-12-25 14:35:04 -0800

    Update expense_form_process.js

--- public_html/includes/custom/expense_form_process.js
+++ public_html/includes/custom/expense_form_process.js
@@ -1,8 +1,13 @@
 jQuery( document ).ready(function() {
-    jQuery( "#item_amount_1,#item_amount_2,#item_amount_3,#item_amount_4,#item_amount_5,#item_amount_6,#item_amount_7,#item_amount_8,#item_amount_9,#item_amount_10, #,#tax" ).keyup(function() {
-    var total = addAmountFields();
-    jQuery("#item_total").val( total.toFixed(2) );
-    });
+    var i = 0;
+    while(jQuery( "#item_amount_"+i).length>0){
+        jQuery( "#item_amount_"+i ).keyup(function() {
+            var total = addAmountFields();
+            jQuery("#item_total").val( total.toFixed(2) );
+        });  
+        i++;
+    }
+    
 });
 function addAmountFields(){
     var sum = 0;
