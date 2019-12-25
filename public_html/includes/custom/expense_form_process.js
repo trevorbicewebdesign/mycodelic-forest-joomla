ae9dbcdf3f14a41a5fca88af695dc51e9a8726f7
Trevor Bice <trevorbicewebdesign@gmail.com>
2019-12-25 14:28:33 -0800

    Update expense_form_process.js

--- public_html/includes/custom/expense_form_process.js
+++ public_html/includes/custom/expense_form_process.js
@@ -1,24 +1,18 @@
-
 jQuery( document ).ready(function() {
-    /*
-     jQuery( "#item_amount_1,#item_amount_2,#item_amount_3,#item_amount_4" ).change(function() {
-         var total = addAmountFields();
-         jQuery("#item_total").val(  total.toFixed(2) );
-         //jQuery(this).val(total.toFixed(2));
-     });
-*/
-     jQuery( "#item_amount_1,#item_amount_2,#item_amount_3,#item_amount_4,#tax" ).keyup(function() {
-         var total = addAmountFields();
-         jQuery("#item_total").val( total.toFixed(2) );
-     });
+    jQuery( "#item_amount_1,#item_amount_2,#item_amount_3,#item_amount_4,#item_amount_5,#item_amount_6,#item_amount_7,#item_amount_8,#item_amount_9,#item_amount_10, #,#tax" ).keyup(function() {
+    var total = addAmountFields();
+    jQuery("#item_total").val( total.toFixed(2) );
+    });
 });
 function addAmountFields(){
     var sum = 0;
-    
-    sum += parseFloat(jQuery( "#item_amount_1").val())?parseFloat(jQuery( "#item_amount_1").val()):0;
-    sum += parseFloat(jQuery( "#item_amount_2").val())?parseFloat(jQuery( "#item_amount_2").val()):0;
-    sum += parseFloat(jQuery( "#item_amount_3").val())?parseFloat(jQuery( "#item_amount_3").val()):0;
-    sum += parseFloat(jQuery( "#item_amount_4").val())?parseFloat(jQuery( "#item_amount_4").val()):0;
+
+    var i = 0;
+    while( amount = parseFloat(jQuery( "#item_amount_"+i).val() ) ) {
+        sum += parseFloat(jQuery( "#item_amount_"+i).val())?parseFloat(jQuery( "#item_amount_"+i).val()):0;
+        i++;
+    }
+
     sum += parseFloat(jQuery( "#tax").val())?parseFloat(jQuery( "#tax").val()):0;
     
     return sum;
