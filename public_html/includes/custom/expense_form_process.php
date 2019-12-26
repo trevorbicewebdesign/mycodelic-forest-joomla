d68caea402f647d8e0051f3cca8ce1a9c79f4088
Trevor Bice <trevorbicewebdesign@gmail.com>
2019-12-25 16:24:58 -0800

    Update expense_form_process.php

--- public_html/includes/custom/expense_form_process.php
+++ public_html/includes/custom/expense_form_process.php
@@ -4,17 +4,16 @@ $names = explode(" ",$user->name);
 // Set the user email
 $_POST['form']['user_email'] = $user->email;
 
-$total = 
-      floatval($_POST['form']['item_amount_1']) 
-    + floatval($_POST['form']['item_amount_2'])
-    + floatval($_POST['form']['item_amount_3'])
-    + floatval($_POST['form']['item_amount_4'])
-    + floatval($_POST['form']['tax'])
-;
-$_POST['form']['item_amount_1'] = empty($_POST['form']['item_amount_1'])? "": number_format($_POST['form']['item_amount_1'], 2);
-$_POST['form']['item_amount_2'] = empty($_POST['form']['item_amount_2'])? "": number_format($_POST['form']['item_amount_2'], 2);
-$_POST['form']['item_amount_3'] = empty($_POST['form']['item_amount_3'])? "": number_format($_POST['form']['item_amount_3'], 2);
-$_POST['form']['item_amount_4'] = empty($_POST['form']['item_amount_4'])? "": number_format($_POST['form']['item_amount_4'], 2);
+$i = 1;
+while($_POST['form']['item_amount_'.$i]){
+    $total += floatval($_POST['form']['item_amount_'.$i]) 
+        
+    $_POST['form']['item_amount_'.$i] = empty($_POST['form']['item_amount_'.$i])? "": number_format($_POST['form']['item_amount_'.$i], 2);
+    
+    $i++;
+}
+$total += floatval($_POST['form']['tax']);
+
 $_POST['form']['tax'] = empty($_POST['form']['tax'])? "": number_format($_POST['form']['tax'], 2);
 
 $_POST['form']['item_total'] = number_format($total,2);
