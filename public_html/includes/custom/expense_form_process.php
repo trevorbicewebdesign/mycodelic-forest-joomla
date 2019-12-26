91ed50802a7aa674262a2885bdb82152bbcb90a1
Trevor Bice <trevorbicewebdesign@gmail.com>
2019-12-25 19:27:07 -0800

    Update expense_form_process.php

--- public_html/includes/custom/expense_form_process.php
+++ public_html/includes/custom/expense_form_process.php
@@ -6,7 +6,7 @@ $_POST['form']['user_email'] = $user->email;
 
 $i = 1;
 while($_POST['form']['item_amount_'.$i]){
-    $total += floatval($_POST['form']['item_amount_'.$i]) 
+    $total += floatval($_POST['form']['item_amount_'.$i]);
         
     $_POST['form']['item_amount_'.$i] = empty($_POST['form']['item_amount_'.$i])? "": number_format($_POST['form']['item_amount_'.$i], 2);
     
