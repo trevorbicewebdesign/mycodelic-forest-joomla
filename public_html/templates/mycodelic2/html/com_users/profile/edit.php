403d1845753a4800235bc8915355e29b5da6813a
Trevor Bice <trevorbicewebdesign@gmail.com>
2020-03-10 02:54:44 -0700

    Don't load tooltip??

--- public_html/templates/mycodelic2/html/com_users/profile/edit.php
+++ public_html/templates/mycodelic2/html/com_users/profile/edit.php
@@ -12,7 +12,6 @@ defined('_JEXEC') or die;
 JHtml::_('behavior.keepalive');
 JHtml::_('behavior.formvalidator');
 JHtml::_('formbehavior.chosen', 'select');
-JHtml::_('bootstrap.tooltip');
 
 
 // Load user_profile plugin language
