0cc04b868a91175194779372d83e03a69112b136
Trevor Bice <trevorbicewebdesign@gmail.com>
2020-03-12 23:12:51 -0700

    Update default_core.php

--- public_html/templates/mycodelic2/html/com_users/profile/default_core.php
+++ public_html/templates/mycodelic2/html/com_users/profile/default_core.php
@@ -14,36 +14,35 @@ defined('_JEXEC') or die;
 	<legend>
 		<?php echo JText::_('COM_USERS_PROFILE_CORE_LEGEND'); ?>
 	</legend>
-	<dl class="dl-horizontal">
-		<dt>
-			<?php echo JText::_('COM_USERS_PROFILE_NAME_LABEL'); ?>
-		</dt>
-		<dd>
-			<?php echo $this->escape($this->data->name); ?>
-		</dd>
-		<dt>
-			<?php echo JText::_('COM_USERS_PROFILE_USERNAME_LABEL'); ?>
-		</dt>
-		<dd>
-			<?php echo $this->escape($this->data->username); ?>
-		</dd>
-		<dt>
-			<?php echo JText::_('COM_USERS_PROFILE_REGISTERED_DATE_LABEL'); ?>
-		</dt>
-		<dd>
-			<?php echo JHtml::_('date', $this->data->registerDate, JText::_('DATE_FORMAT_LC1')); ?>
-		</dd>
-		<dt>
-			<?php echo JText::_('COM_USERS_PROFILE_LAST_VISITED_DATE_LABEL'); ?>
-		</dt>
-		<?php if ($this->data->lastvisitDate != $this->db->getNullDate()) : ?>
-			<dd>
-				<?php echo JHtml::_('date', $this->data->lastvisitDate, JText::_('DATE_FORMAT_LC1')); ?>
-			</dd>
-		<?php else : ?>
-			<dd>
-				<?php echo JText::_('COM_USERS_PROFILE_NEVER_VISITED'); ?>
-			</dd>
-		<?php endif; ?>
-	</dl>
+	<ul class="dl-horizontal">
+		<li>
+            <label><?php echo JText::_('COM_USERS_PROFILE_NAME_LABEL'); ?>:</label>
+			<div>
+		        <?php echo $this->escape($this->data->name); ?>
+            </div>
+        </li>
+        <li>
+            <label><?php echo JText::_('COM_USERS_PROFILE_USERNAME_LABEL'); ?>:</label>
+			<div>
+		        <?php echo $this->escape($this->data->username); ?>
+            </div>
+        </li>
+		<li>
+            <label><?php echo JText::_('COM_USERS_PROFILE_REGISTERED_DATE_LABEL'); ?>:</label>
+			<div>
+		        <?php echo JHtml::_('date', $this->data->registerDate, JText::_('DATE_FORMAT_LC1')); ?>
+            </div>
+        </li>
+        <li>
+            <label><?php echo JText::_('COM_USERS_PROFILE_LAST_VISITED_DATE_LABEL'); ?>:</label>
+            <div>
+            <?php if ($this->data->lastvisitDate != $this->db->getNullDate()) : ?>
+		        <?php echo JHtml::_('date', $this->data->lastvisitDate, JText::_('DATE_FORMAT_LC1')); ?>
+            <?php else : ?>
+                <?php echo JText::_('COM_USERS_PROFILE_NEVER_VISITED'); ?>
+            <?php endif; ?>
+            </div>
+        </li>
+       
+	</ul>
 </fieldset>
