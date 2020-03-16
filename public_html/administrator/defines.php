9048c1735826f549ccce4d716d8989c7c76f7c4f
Trevor Bice <trevorbicewebdesign@gmail.com>
2020-03-15 18:53:07 -0700

    Correct defines

--- public_html/administrator/defines.php
+++ public_html/administrator/defines.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @package    Joomla.Site
+ * @package    Joomla.Administrator
  *
- * @copyright  Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @copyright  Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -21,15 +21,15 @@ if(CODECEPTION){$environment = 'codeception';}
 $parts = explode(DIRECTORY_SEPARATOR, JPATH_BASE);
 array_pop($parts);
 
-// Defines.
+// Defines
 define('JPATH_ROOT',          implode(DIRECTORY_SEPARATOR, $parts));
 define('JPATH_SITE',          JPATH_ROOT);
 $public_html = dirname(JPATH_ROOT, 1);
 define('JPATH_CONFIGURATION', $public_html . DIRECTORY_SEPARATOR . 'configurations' . DIRECTORY_SEPARATOR .$environment);
-define('JPATH_ADMINISTRATOR', $public_html . DIRECTORY_SEPARATOR . 'public_html' . DIRECTORY_SEPARATOR . 'administrator');
-define('JPATH_LIBRARIES',     $public_html . DIRECTORY_SEPARATOR . 'public_html' . DIRECTORY_SEPARATOR . 'libraries');
-define('JPATH_PLUGINS',       $public_html . DIRECTORY_SEPARATOR . 'public_html' . DIRECTORY_SEPARATOR . 'plugins');
-define('JPATH_INSTALLATION',  $public_html . DIRECTORY_SEPARATOR . 'public_html' . DIRECTORY_SEPARATOR . 'installation');
-define('JPATH_THEMES',        $public_html . DIRECTORY_SEPARATOR . 'public_html' . DIRECTORY_SEPARATOR . 'templates');
-define('JPATH_CACHE',         $public_html . DIRECTORY_SEPARATOR . 'public_html' . DIRECTORY_SEPARATOR . 'cache');
+define('JPATH_ADMINISTRATOR', JPATH_ROOT . DIRECTORY_SEPARATOR . 'administrator');
+define('JPATH_LIBRARIES',     JPATH_ROOT . DIRECTORY_SEPARATOR . 'libraries');
+define('JPATH_PLUGINS',       JPATH_ROOT . DIRECTORY_SEPARATOR . 'plugins');
+define('JPATH_INSTALLATION',  JPATH_ROOT . DIRECTORY_SEPARATOR . 'installation');
+define('JPATH_THEMES',        JPATH_BASE . DIRECTORY_SEPARATOR . 'templates');
+define('JPATH_CACHE',         JPATH_BASE . DIRECTORY_SEPARATOR . 'cache');
 define('JPATH_MANIFESTS',     JPATH_ADMINISTRATOR . DIRECTORY_SEPARATOR . 'manifests');
