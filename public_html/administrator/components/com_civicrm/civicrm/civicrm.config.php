b36482c645eb286a9c1a2105e8567768aa6f0667
Trevor Bice <trevorbicewebdesign@gmail.com>
2020-06-01 14:53:06 -0700

    Update civicrm.config.php

--- public_html/administrator/components/com_civicrm/civicrm/civicrm.config.php
+++ public_html/administrator/components/com_civicrm/civicrm/civicrm.config.php
@@ -1,12 +1,13 @@
 <?php
-define('CIVICRM_SETTINGS_PATH', '/home/trevorbi/mycodelicforest.org/public_html/administrator/components/com_civicrm/civicrm.settings.php');
-$error = @include_once( '/home/trevorbi/mycodelicforest.org/public_html/administrator/components/com_civicrm/civicrm.settings.php' );
+$basepath = dirname(__FILE__, 5);  
+define('CIVICRM_SETTINGS_PATH', "{$basepath}administrator/components/com_civicrm/civicrm.settings.php");
+$error = include_once( "{$basepath}administrator/components/com_civicrm/civicrm.settings.php" );
 if ( $error == false ) {
-    echo "Could not load the settings file at: /home/trevorbi/mycodelicforest.org/public_html/administrator/components/com_civicrm/civicrm.settings.php
-";
+    echo "Could not load the settings file at: {$basepath}administrator/components/com_civicrm/civicrm.settings.php";
     exit( );
 }
 
 // Load class loader
 require_once $civicrm_root . '/CRM/Core/ClassLoader.php';
+
 CRM_Core_ClassLoader::singleton()->register();
